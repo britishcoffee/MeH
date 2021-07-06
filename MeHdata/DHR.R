9007ead9724cf6caf1a7e82165dadc80c49ff8a4
britishcoffee <ytchang.sabrina@gmail.com>
2021-07-06 16:00:50 +0800

    Update DHR.R

--- MeHdata/DHR.R
+++ MeHdata/DHR.R
@@ -6,7 +6,7 @@ install.packages("foreach")
 library(foreach)
 
 MeH.t=function(vector,conditions,compare) {
-  ind1<-which(conditions == compare[1])+3 # +2 for chrom,bin and strand columns
+  ind1<-which(conditions == compare[1])+3 # +3 for chrom,bin and strand columns
   ind2<-which(conditions == compare[2])+3
   #l=length(vector)
   vector=as.data.frame(vector)
@@ -21,38 +21,30 @@ MeH.t=function(vector,conditions,compare) {
   }
 }
 
+findgene = function(position) {
+  chr=as.character(position[1])
+  #message(chr)
+  BP=as.numeric(position[2])
+  #message(BP)
+  St=as.character(position[3])
+  Gene=geneloc$gene[which((geneloc$TSS<=BP)*(geneloc$TES>=BP)*(as.character(geneloc$chrom)==chr)*(as.character(geneloc$strand)==as.character(St))==1)][1]
+  if (St=='f') {
+    promoter=geneloc$gene[which((geneloc$TSS-1000<=BP)*(geneloc$TSS+1000>=BP)*(as.character(geneloc$chrom)==chr)*(geneloc$strand=="f")==1)][1]
+  }
+  if (St=='r') {
+    promoter=geneloc$gene[which((geneloc$TES-1000<=BP)*(geneloc$TES+1000>=BP)*(as.character(geneloc$chrom)==chr)*(geneloc$strand=="r")==1)][1]
+  }
+  return(list(chrom=chr,bin=BP,Gene=Gene,Promoter=promoter,strand=St))
+}
+
 #### Load files for analysis by first setting the work directory to where your files are located
 setwd("~/MeHdata")
-Dest <- read.table('CG_Results.csv',header=TRUE,sep=",")
-Dest <- read.table('CHG_Results.csv',header=TRUE,sep=",")
-Dest <- read.table('CHH_Results.csv',header=TRUE,sep=",")
+CG <- read.table('CG_Results.csv',header=TRUE,sep=",")
+CHG <- read.table('CHG_Results.csv',header=TRUE,sep=",")
+CHH <- read.table('CHH_Results.csv',header=TRUE,sep=",")
 
-#### Remove rows with no data
-Dest=Dest[which(apply(Dest,1,function(x) sum(is.na(x)))==0),]
-
-#### Construct bins of 400bp (default, can be changed to any even number) and add a column of bin position to data
-bin_size=400
-Dest$bin<-((Dest$pos-1) %/% bin_size)*bin_size+(bin_size/2)
-
-#### Obtain names of samples (will be identical to names of the bam files provided if unchanged)
-samples=colnames(Dest)[which(!colnames(Dest) %in% c("chrom","pos","strand","bin"))]
-
-#### Obtain results (average methylation heterogeneity for the bins) by taking averages of methylation heterogeneity for windows within the same bins
-
-# assign("[samplename]",data.frame(Dest %>% group_by(chrom,bin,strand) %>% summarise(mean(samplename))))
-assign("C0031test1234",data.frame(Dest %>% group_by(chrom,bin,strand) %>% summarise(mean(C0031test1234))))
-assign("C0033test1234",data.frame(Dest %>% group_by(chrom,bin,strand) %>% summarise(mean(C0033test1234))))
-assign("C0035test1234",data.frame(Dest %>% group_by(chrom,bin,strand) %>% summarise(mean(C0035test1234))))
-assign("C0037test1234",data.frame(Dest %>% group_by(chrom,bin,strand) %>% summarise(mean(C0037test1234))))
-
-#### Merging results from different samples into a matrix called "new"
-new=c()
-for (s in samples) {
-  data=get(s)
-  colnames(data)[4]=s
-  if (!is.null(new)) new = merge(new,data,by=c("chrom","bin","strand"))
-  else new=data
-}
+#### Remove rows with missing data
+CG=CG[which(apply(CG,1,function(x) sum(is.na(x)))==0),]
 
 #### Define conditions of all samples; i.e., A and B for 2 conditions, each with two replicates, samples 1 and 2 are replicates of A and samples 3 and 4 are replicates for B. This is for comparisons to be carried out later on
 conditions <- c("A","A","B","B")
@@ -61,15 +53,17 @@ conditions <- c("A","A","B","B")
 library(doParallel)
 registerDoParallel(cores=4)
 # Compare condition B with A
-Comp1<-data.frame(foreach(i = 1:dim(new)[1],.combine = rbind) %dopar% 
-                    MeH.t(new[i,],conditions=conditions,c("A","B")))
+Comp1<-data.frame(foreach(i = 1:dim(CG)[1],.combine = rbind) %dopar% 
+                    MeH.t(CG[i,],conditions=conditions,c("A","B")))
+Comp1$padj=p.adjust(Comp1$pvalue)
 
 #### Select differential heterogeneous regions based on user specified conditions; i.e., p-value of 0.05 and delta of 1 (positive or negative)
-Comp1$DHR <- (Comp1$pvalue<0.05)*(abs(Comp1$delta)>1)
+Comp1$DHR <- (Comp1$padj<0.05)*(Comp1$pvalue<0.05)*(abs(Comp1$delta)>1.4)
 
 #### DHG analysis if bed file is given as .txt with each row representing a gene and consists of gene name, chromosome number, TSS, TES and strand as 'f' (forward) or 'r' (reverse)
-geneloc<-read.table('../hg19plusgenes.txt',header=FALSE)
+geneloc<-read.table('genelist.txt',header=TRUE)
+colnames(geneloc)<-c("gene","chrom","strand","TSS","TES")
+geneloc$strand[as.character(geneloc$strand)=="+"]<-"f"
+geneloc$strand[as.character(geneloc$strand)=="-"]<-"r"
 
-genelist<-foreach(i = 1:dim(Comp1)[1],.combine = rbind) %dopar% findgene(Comp1[i,c("chrom","bin","strand")]) 
-
-
+genelist<-as.data.frame(foreach(i = 1:dim(Comp1)[1],.combine = rbind) %dopar% findgene(Comp1[i,c("chrom","bin","strand")]))
