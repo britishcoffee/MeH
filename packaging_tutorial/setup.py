d940995d43244adf0ab6da590d609d1ee27f5197
britishcoffee <ytchang.sabrina@gmail.com>
2021-08-02 17:29:11 +0800

    Update setup.py

--- packaging_tutorial/setup.py
+++ packaging_tutorial/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.2' 
 DESCRIPTION = 'A bioinformatic tool for profiling methylation heterogeneity genomewide'
 LONG_DESCRIPTION = ''
 
