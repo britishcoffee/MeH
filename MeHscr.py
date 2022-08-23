789648bac3a384491d1cba14c0f6ae69b5ebe359
Sabrina Ya-Ting Chang <ytchang.sabrina@gmail.com>
2022-08-23 12:07:05 +0800

    Update MeHscr.py

--- MeHscr.py
+++ MeHscr.py
@@ -393,7 +393,7 @@ def CGgenome_scr(bamfile,w,fa,optional,melv,silence=False,dist=1,MeH=2,imp=True)
                         ['chrom','pos','p01','p02','p03','p04','p05','p06','p07','p08','p09','p10','p11','p12','p13','p14','p15','p16'\
                         ,'p17','p18','p19','p20','p21','p22','p23','p24','p25','p26','p27','p28',\
                         'p29','p30','p31','p32','MeH','dis','strand'])
-        if w==5:
+        if w==6:
             Resultopt = pd.DataFrame(columns=\
                         ['chrom','pos','p01','p02','p03','p04','p05','p06','p07','p08','p09','p10','p11','p12','p13','p14','p15','p16'\
                         ,'p17','p18','p19','p20','p21','p22','p23','p24','p25','p26','p27','p28',\
@@ -709,7 +709,7 @@ def CHHgenome_scr(bamfile,w,fa,optional,melv,silence=False,dist=1,MeH=2,imp=True
                         ['chrom','pos','p01','p02','p03','p04','p05','p06','p07','p08','p09','p10','p11','p12','p13','p14','p15','p16'\
                         ,'p17','p18','p19','p20','p21','p22','p23','p24','p25','p26','p27','p28',\
                         'p29','p30','p31','p32','MeH','dis','strand'])
-        if w==5:
+        if w==6:
             Resultopt = pd.DataFrame(columns=\
                         ['chrom','pos','p01','p02','p03','p04','p05','p06','p07','p08','p09','p10','p11','p12','p13','p14','p15','p16'\
                         ,'p17','p18','p19','p20','p21','p22','p23','p24','p25','p26','p27','p28',\
@@ -1010,7 +1010,7 @@ def CHGgenome_scr(bamfile,w,fa,optional,melv,silence=False,dist=1,MeH=2,imp=True
                         ['chrom','pos','p01','p02','p03','p04','p05','p06','p07','p08','p09','p10','p11','p12','p13','p14','p15','p16'\
                         ,'p17','p18','p19','p20','p21','p22','p23','p24','p25','p26','p27','p28',\
                         'p29','p30','p31','p32','MeH','dis','strand'])
-        if w==5:
+        if w==6:
             Resultopt = pd.DataFrame(columns=\
                         ['chrom','pos','p01','p02','p03','p04','p05','p06','p07','p08','p09','p10','p11','p12','p13','p14','p15','p16'\
                         ,'p17','p18','p19','p20','p21','p22','p23','p24','p25','p26','p27','p28',\
